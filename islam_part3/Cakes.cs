988d3eb7639c518e160c61bfca8801a1597327f1
FraintSaad <islamqwertyu@mail.ru>
2023-10-25 17:09:22 +0300

    com

---
+++ islam_part3/Cakes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace islam_part3
+{
+    internal class Cakes
+    {
+        public static void Start()
+        {
+            Console.WriteLine("В магазине три торта: Тортик, стоит 1507 рублей, Тортище, стоит 3600 рублей, Торт95, стоит 200 рублей");
+            string[] cakes = new string[] { "Тортик", "Тортище", "Торт95", "Тортовик"};
+            int[] prices = new int[] { 1507, 3600, 200, 600 };
+            int choice = -1;
+            do
+            {
+                Console.WriteLine("Введите номер торта (1-3):");
+                string input = Console.ReadLine();
+                try
+                {
+                    choice = Convert.ToInt32(input) - 1;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Некорректный ввод!");
+                }
+                if (choice < 0 || choice >= cakes.Length)
+                {
+                    Console.WriteLine("Тортиньо яц, нету, досвидания");
+                }
+            } while (choice < 0 || choice >= cakes.Length);
+         
+                if (choice <= 3)
+                {
+                    int price = prices[choice];
+                    Console.WriteLine($"Торт: {cakes[choice]}\nЦена: {price}");
+                }
+                else
+                {
+                    Console.WriteLine("Тортиньо яц, нету, досвидания");
+                }
+            }
+        }
+    }
+
--- islam_part3/Program.cs
+++ islam_part3/Program.cs
@@ -6,43 +6,11 @@ namespace islam_part3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("В магазине три торта: Тортик, стоит 1507 рублей, Тортище, стоит 3600 рублей, Торт95, стоит 200 рублей");
-            Console.WriteLine("1.Тортик");
-            int Тортик = 1507;
-            Console.WriteLine("2.Тортище"); 
-            int Тортище = 3600;
-            Console.WriteLine("3.Торт95");
-            int Торт95 = 200;
-            int Тортиньо = 0;
-            Console.WriteLine("Муьлха торт еза хьуна?");
-            int a=Convert.ToInt32(Console.ReadLine());
-            int choice = 0;
-            switch (a)
-            {
-                case 1:
-                    choice = Тортик;
-                    Console.WriteLine("Тортик");
-                    Console.WriteLine($"Тортиках {choice} сом доьху");
-                    break;
-                case 2:
-                    choice = Тортище;
-                    Console.WriteLine("Тортище");
-                    Console.WriteLine($"Тортищах {choice} сом доьху");
-                    break;
-                case 3:
-                    choice = Торт95;
-                    Console.WriteLine("Торт95");
-                    Console.WriteLine($"Торт95 {choice} соьманах ю");
-                    break;
-                    case 4:
-                    choice = Тортиньо;
-                    Console.WriteLine("Тортиньо");
-                    Console.WriteLine("Тортиньо йац, нету, давай досвидания");
-                    break;
-            }
-
-
-
+            Cakes.Start();
         }
+
+
+
+        
     }
 }
