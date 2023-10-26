458ee41a330182d484b9c16de1e782a8b9f7e4d9
FraintSaad <islamqwertyu@mail.ru>
2023-10-26 15:31:32 +0300

    Com3

--- islam_part3/Cakes.cs
+++ islam_part3/Cakes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,29 +11,19 @@ namespace islam_part3
     {
         public static void Start()
         {
-            Console.WriteLine("В магазине три торта: Тортик, стоит 1507 рублей, Тортище, стоит 3600 рублей, Торт95, стоит 200 рублей");
-            string[] cakes = new string[] { "Тортик", "Тортище", "Торт95", "Тортовик"};
-            int[] prices = new int[] { 1507, 3600, 200, 600 };
+            
+            string[] cakes = new string[] { "Тортик", "Тортище", "Торт95"};
+            Console.WriteLine($"В наличии есть: {string.Join(", ", cakes)}");
+            int[] prices = new int[] { 1507, 3600, 200 };
             int choice = -1;
-            do
             {
-                Console.WriteLine("Введите номер торта (1-3):");
+                Console.WriteLine("Выберите торт:");
                 string input = Console.ReadLine();
-                try
-                {
-                    choice = Convert.ToInt32(input) - 1;
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Некорректный ввод!");
-                }
-                if (choice < 0 || choice >= cakes.Length)
-                {
-                    Console.WriteLine("Тортиньо яц, нету, досвидания");
-                }
-            } while (choice < 0 || choice >= cakes.Length);
-         
-                if (choice <= 3)
+                choice = Convert.ToInt32(input) - 1;
+            }
+
+
+            if (choice <= 2 && choice >= 0)
                 {
                     int price = prices[choice];
                     Console.WriteLine($"Торт: {cakes[choice]}\nЦена: {price}");
