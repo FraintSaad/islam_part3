c25e6fac670fc4c75bddba5aa474b65548b99030
FraintSaad <islamqwertyu@mail.ru>
2023-11-09 22:04:52 +0300

    Com2 Weather

--- islam_part3/WeatherInformation.cs
+++ islam_part3/WeatherInformation.cs
@@ -12,32 +12,34 @@ namespace islam_part3
         public static void Start()
         {
             int[] temperatures = {5, 7, 1, -3, 6, 9, -5, 0, 2};
-            int[] precipitation = {30, 0, 50, 30, 40, 60, 0, 20, 30};
-            int sum = 0;
+            int[] precipitation = {30, 0, 30, 50, 40, 60, 80, 20, 30};
+            int sumTemp = 0;
 
             for(int i=0; i<temperatures.Length; i++)
             {
-                sum += temperatures[i];
+                sumTemp += temperatures[i];
             }
-            int mid = sum / temperatures.Length;
+            int mid = sumTemp / temperatures.Length;
             Console.WriteLine("Средняя месячная температура: " + mid);
 
-            int hot = temperatures.Max();
-            int hIndex = temperatures.ToList().IndexOf(hot);
+            var temperaturesList = temperatures.ToList();
+            int hot = temperaturesList.Max();
+            int hIndex = temperaturesList.IndexOf(hot);
             Console.WriteLine("Самый теплый день месяца: " + hIndex);
 
             int cold = temperatures.Min();
-            int cIndex = temperatures.ToList().IndexOf(cold);
+            int cIndex = temperaturesList.IndexOf(cold);
             Console.WriteLine("Самый теплый день месяца: " + cIndex);
 
             int FullPrec = precipitation.Sum();
             Console.WriteLine("Общее количество осадков за месяц: " + FullPrec);
 
+            int sumPer = 0;
             for(int i=0; i<precipitation.Length; i++)
             {
-                sum += precipitation[i];
+                sumPer += precipitation[i];
             }
-            int centr = sum / precipitation.Length;
+            int centr = sumPer / precipitation.Length;
             int count = 0;
             for(int i = 0; i<precipitation.Length; i++)
             {
@@ -45,27 +47,18 @@ namespace islam_part3
             }
             Console.WriteLine("Количество дней с осадками выше среднего: " + count);
 
-            string freezeDays = "";
+            string freezeAndIncreasingPrecipitation = "";
+            
             for (int i = 0; i < temperatures.Length; i++)
             {
-                if (temperatures[i] < 0)
-                { freezeDays += i + " "; }    
-                
-            }
-            string increasingPrecipitationDays = "";
-            for (int i=1; i< precipitation.Length; i++)
-            {
-                if (precipitation[i] > precipitation[i-1])
-                { increasingPrecipitationDays += i + " "; }    
-                
+                if (temperatures[i] < 0 && precipitation[i] > precipitation[i - 1])
+                { 
+                    freezeAndIncreasingPrecipitation += i + " ";
+                }
+
             }
 
-            Console.WriteLine($"Дни, в которых была заморозка: " + freezeDays);
-             Console.WriteLine($"Дни с увеличением осадков по сравнению с предыдущим днем: " + increasingPrecipitationDays); 
-
-
-
-
+            Console.WriteLine($"Дни, в которых была заморозка и увеличение осадков: " + freezeAndIncreasingPrecipitation);
 
         }
     }
